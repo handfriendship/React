e76fa523f02fb4d0a9c757f5fd0bbcb8fc7140ed
handfriendship <sonwj77@naver.com>
2019-08-23 16:47:14 +0900

    정답 안보고 만든 교수목록 프로젝트

--- src/App.js
+++ src/App.js
@@ -7,74 +7,65 @@ import PlannerPage from "./pages/planner";
 import AboutPage from "./pages/about";
 
 const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+const { Header, Content, Sider , Footer} = Layout;
 
 function App() {
+    const nowPage = window.location.href;
     return (
         <BrowserRouter>
             <Layout>
                 <Header className="header">
-                    <div className="logo" />
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['2']}
-                        style={{ lineHeight: '64px' }}
-                    >
-                        <Menu.Item key="1">
-                            <Link to="/professor">
-                                교수 목록
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Link to="/planner">
-                                교수 계획
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Link to="/about">
-                                About
-                            </Link>
-                        </Menu.Item>
-                    </Menu>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={[nowPage]}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key="/professor"><Link to="/professor">교수 목록</Link></Menu.Item>
+                    <Menu.Item key="/planner"><Link to="/planner">교수 계획</Link></Menu.Item>
+                    <Menu.Item key="about"><Link to="/about">About</Link></Menu.Item>
+                </Menu>
                 </Header>
                 <Layout>
-                    <Sider width={200} style={{ background: '#fff' }}>
-                        <Menu
-                            mode="inline"
-                            defaultSelectedKeys={['1']}
-                            defaultOpenKeys={['sub1']}
-                            style={{ height: '100%', borderRight: 0 }}>
-                            <SubMenu key="sub1"
-                                title={<span><Icon type="user" />subnav 1</span>}>
-                                <Menu.Item key="1">option1</Menu.Item>
-                                <Menu.Item key="2">option2</Menu.Item>
-                            </SubMenu>
-                        </Menu>
-                    </Sider>
-                    <Layout style={{ padding: '0 24px 24px' }}>
-                        <Breadcrumb style={{ margin: '16px 0' }}>
-                            <Breadcrumb.Item>Home</Breadcrumb.Item>
-                            <Breadcrumb.Item>List</Breadcrumb.Item>
-                            <Breadcrumb.Item>App</Breadcrumb.Item>
-                        </Breadcrumb>
-                        <Content
-                            style={{
-                                background: '#fff',
-                                padding: 24,
-                                margin: 0,
-                                minHeight: 280,
-                            }}
+                <Sider width={200} style={{ background: '#fff' }}>
+                    <Menu
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    defaultOpenKeys={['sub1']}
+                    style={{ height: '100%', borderRight: 0 }}
+                    >
+                        <SubMenu
+                            key="sub1"
+                            title={
+                            <span>
+                                <Icon type="user" />
+                                subnav 1
+                            </span>
+                            }
                         >
-                            <Switch>
-                                <Route exact path="/professor" component={ProfessorListPage} />
-                                <Route exact path="/planner" component={PlannerPage} />
-                                <Route exact path="/about" component={AboutPage} />
-                            </Switch>
-                        </Content>
-                    </Layout>
+                            <Menu.Item key="1">option1</Menu.Item>
+                            <Menu.Item key="2">option2</Menu.Item>
+                            
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout style={{ padding: '0 24px 24px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Content style={{
+                        background: '#fff',padding: 24,margin: 0, minHeight: 280,}} >
+                        <Route path="/professor" component={ProfessorListPage}></Route>
+                        <Route path="/planner" component={PlannerPage}></Route>
+                        <Route path="/about" component={AboutPage}></Route>
+                    </Content>
+                </Layout>
                 </Layout>
             </Layout>
+            
         </BrowserRouter>
     );
 }
--- src/pages/professor_list.js
+++ src/pages/professor_list.js
@@ -3,32 +3,62 @@ import {useEffect, useState} from 'react';
 import axios from 'axios';
 import {PageHeader, Table} from 'antd';
 
-const columns = [
-    { title: '이름', dataIndex: 'name', key: 'name' },
-    { title: '전화번호', dataIndex: 'phone', key: 'phone'}
-];
+const url = "http://localhost:8000/univ/professor.json";
 
-const ProfessorListPage = () => {
-    const [professorList, setProfessorList] = useState([]);
+class ProfessorListPage extends React.Component {
+    
+    state = {
+        dataSource: []
+    }
 
-    useEffect(() => {
-        const fetchList = async () => {
-            const url = "http://localhost:8000/univ/professor.json";
-            const {data} = await axios.get(url);
-            setProfessorList(data);
+    componentDidMount = async () => {
+        const {data} = await axios.get(url);
+        console.log(data); 
+        const temp = [];
+        data.map(element => (
+            this.setState({
+                dataSource: [...this.state.dataSource].concat({name:element.name, phone:element.phone})
+            })
+        ))
+
+    }
+/*
+    dataSource = [
+       
+        {
+          key: '1',
+          'name': 'Mike', // 여기의 name은 dataIndex랑 매칭되는 듯?
+          'phone': 32
+        },
+        {
+          key: '2',
+          '이름': 'John',
+          '전화번호': 42
+          
+        },
+      ];
+      */ 
+      columns = [ //columns 안에 {} 이거 한 개가 열 한개를 말함.
+        {
+          title: '이름',
+          dataIndex: 'name',
+          key: 'name',
+        }, 
+        {
+          title: '전화번호',
+          dataIndex: 'phone',
+          key: 'phoneNum',
         }
-        fetchList();
-    }, []);
-
-    return (
-        <div>
-            <PageHeader title="교수목록" subTitle="subTitle" />
-
-            <Table columns={columns} dataSource={professorList}
-                    pagination={false}
-            />
-        </div>
-    );
+      ];
+     
+    render() {
+        return (
+            <>
+                <PageHeader title="교수 목록" subTitle="subTitle" />
+                <Table dataSource={this.state.dataSource} columns={this.columns} />
+            </>
+        );
+    }
 }
 
 export default ProfessorListPage;
